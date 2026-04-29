10c01473c1117163e50f49811eb98097e7fbf27d
hasankucukali <96529211+hasankucukali@users.noreply.github.com>
2026-04-29 18:14:48 +0300

    app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ def excel_yukle():
     try:
         file = request.files["file"]
 
-        df = pd.read_excel(file)
+        df = pd.read_excel(file, engine="openyxl")
 
         con = get_db()
         cur = con.cursor()

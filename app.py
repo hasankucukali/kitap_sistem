c75f364b18f54b73587c1fd90ed7bec34ebff5e1
hasankucukali <96529211+hasankucukali@users.noreply.github.com>
2026-04-30 16:50:32 +0300

    app.py

--- app.py
+++ app.py
@@ -327,7 +327,7 @@ def tamamla():
 @app.route("/fis_sor")
 def fis_sor():
     session["mesaj"] = "Satış tamamlandı ✅"
-    return render_template("fis_sor.html"
+    return render_template("fis_sor.html")
 
 from datetime import datetime
 

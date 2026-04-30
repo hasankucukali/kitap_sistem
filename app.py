562f0267d93789ce8dc5efa9338a4e5b20b6d4ed
hasankucukali <96529211+hasankucukali@users.noreply.github.com>
2026-04-30 10:50:17 +0300

    app.py

--- app.py
+++ app.py
@@ -222,29 +222,46 @@ def satis():
 
 
 # ➕ ➖ ❌
-@app.route("/arttir/<barkod>")
-def arttir(barkod):
-    for i in session["sepet"]:
-        if i["barkod"] == barkod:
-            i["adet"] += 1
-    session.modified = True
-    return redirect("/satis")
+@app.route("/stok_arttir/<barkod>")
+def stok_arttir(barkod):
+    con = get_db()
+    cur = con.cursor()
+
+    cur.execute("UPDATE kitaplar SET stok = stok + 1 WHERE barkod=%s", (barkod,))
+
+    con.commit()
+    cur.close()
+    con.close()
+
+    return redirect("/stok")
 
 
-@app.route("/azalt/<barkod>")
-def azalt(barkod):
-    for i in session["sepet"]:
-        if i["barkod"] == barkod and i["adet"] > 1:
-            i["adet"] -= 1
-    session.modified = True
-    return redirect("/satis")
+@app.route("/stok_azalt/<barkod>")
+def stok_azalt(barkod):
+    con = get_db()
+    cur = con.cursor()
+
+    cur.execute("UPDATE kitaplar SET stok = stok - 1 WHERE barkod=%s AND stok > 0", (barkod,))
+
+    con.commit()
+    cur.close()
+    con.close()
+
+    return redirect("/stok")
 
 
-@app.route("/sil/<barkod>")
-def sil(barkod):
-    session["sepet"] = [i for i in session["sepet"] if i["barkod"] != barkod]
-    session.modified = True
-    return redirect("/satis")
+@app.route("/stok_sil/<barkod>")
+def stok_sil(barkod):
+    con = get_db()
+    cur = con.cursor()
+
+    cur.execute("DELETE FROM kitaplar WHERE barkod=%s", (barkod,))
+
+    con.commit()
+    cur.close()
+    con.close()
+
+    return redirect("/stok")
 
 
 # ✅ SATIŞ TAMAMLA
